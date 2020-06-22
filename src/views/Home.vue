d6d131acaa194938958f37ba9f70021408fb7b00
heykkyu <heykkyu@gmail.com>
2020-06-22 23:46:28 +0900

    how works web socket

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <button @click="sendMessage('Hi, there')">Connect Socket</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      connection: null,
+    }
+  },
+  methods: {
+    sendMessage (message) {
+      console.log(this.connection);
+      this.connection.send(message);
+    }
+  },
+  created () {
+    console.log("Strating Connection to WebSocket Server");
+    this.connection = new WebSocket("wss://echo.websocket.org");
+
+    this.connection.onopen = function(event) {
+      console.log(event);
+      console.log("Successfully Connected to the Echo WebSocket Server");
+    }
+
+    this.connection.onmessage = function(event) {
+      console.log(event);
+    }
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pages: {
+    index: 'src/main.js'
+  },
+  lintOnSave: false
+}
